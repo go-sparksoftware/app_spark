89151ef0f7de5f248385fa9da8d8d18906990a3a
jaysongo <jayson.go@gmail.com>
2024-11-22 08:09:07 -0700

    Added initial MaterialSymbols

---
+++ docs/material-symbols.md
@@ -0,0 +1,15 @@
+# MaterialSymbols
+MaterialSymbols provides icons for the latest Material Icons. To learn more about Google's Material Icons, go here: https://fonts.google.com/icons
+
+## Getting started
+To use MaterialSymbols, add the package and include the package's font in your project
+```
+flutter pub add app_spark
+```
+```yaml
+flutter:
+  fonts:
+    - family: MaterialSymbols
+      fonts:
+        - asset: packages/app_spark/fonts/material_symbols/material_symbols_sharp.ttf
+```
---
+++ lib/material_symbols.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+@staticIconProvider
+abstract class MaterialSymbols {
+  static const accountCircle = IconData(0xe853, fontFamily: "MaterialSymbols");
+  static const add = IconData(0xe145, fontFamily: "MaterialSymbols");
+  static const arrowBack = IconData(0xe5c4, fontFamily: "MaterialSymbols");
+  static const check = IconData(0xe5ca, fontFamily: "MaterialSymbols");
+  static const checkCircle = IconData(0xe86c, fontFamily: "MaterialSymbols");
+  static const calendarMonth = IconData(0xebcc, fontFamily: "MaterialSymbols");
+  static const cancel = IconData(0xe5c9, fontFamily: "MaterialSymbols");
+  static const close = IconData(0xe5cd, fontFamily: "MaterialSymbols");
+  static const darkMode = IconData(0xe51c, fontFamily: "MaterialSymbols");
+  static const dashboard = IconData(0xe871, fontFamily: "MaterialSymbols");
+  static const delete = IconData(0xe872, fontFamily: "MaterialSymbols");
+  static const donutLarge = IconData(0xe917, fontFamily: "MaterialSymbols");
+  static const edit = IconData(0xe3c9, fontFamily: "MaterialSymbols");
+  static const factCheck = IconData(0xf0c5, fontFamily: "MaterialSymbols");
+  static const filterList = IconData(0xe152, fontFamily: "MaterialSymbols");
+  static const foundation = IconData(0xf200, fontFamily: "MaterialSymbols");
+  static const groups = IconData(0xf233, fontFamily: "MaterialSymbols");
+  static const home = IconData(0xe88a, fontFamily: "MaterialSymbols");
+  static const info = IconData(0xe88e, fontFamily: "MaterialSymbols");
+  static const lightMode = IconData(0xe518, fontFamily: "MaterialSymbols");
+  static const login = IconData(0xea77, fontFamily: "MaterialSymbols");
+  static const logout = IconData(0xe9ba, fontFamily: "MaterialSymbols");
+  static const manageAccounts = IconData(0xf02e, fontFamily: "MaterialSymbols");
+  static const map = IconData(0xe55b, fontFamily: "MaterialSymbols");
+  static const moreVert = IconData(0xe5d4, fontFamily: "MaterialSymbols");
+  static const notifications = IconData(0xe7f4, fontFamily: "MaterialSymbols");
+  static const pause = IconData(0xe034, fontFamily: "MaterialSymbols");
+  static const person = IconData(0xe7fd, fontFamily: "MaterialSymbols");
+  static const photoCamera = IconData(0xe412, fontFamily: "MaterialSymbols");
+  static const play = IconData(0xe037, fontFamily: "MaterialSymbols");
+  static const prelimanary = IconData(0xe7d8, fontFamily: "MaterialSymbols");
+  static const priorityHigh = IconData(0xe645, fontFamily: "MaterialSymbols");
+  static const remove = IconData(0xe15b, fontFamily: "MaterialSymbols");
+  static const restartAlt = IconData(0xf053, fontFamily: "MaterialSymbols");
+  static const school = IconData(0xe80c, fontFamily: "MaterialSymbols");
+  static const settings = IconData(0xe8b8, fontFamily: "MaterialSymbols");
+  static const settingsAccountBox =
+      IconData(0xf835, fontFamily: "MaterialSymbols");
+  static const sortByAlpha = IconData(0xe053, fontFamily: "MaterialSymbols");
+  static const sportsMartialArts =
+      IconData(0xeae9, fontFamily: "MaterialSymbols");
+  static const star = IconData(0xe838, fontFamily: "MaterialSymbols");
+  static const summarize = IconData(0xf071, fontFamily: "MaterialSymbols");
+  static const task = IconData(0xf075, fontFamily: "MaterialSymbols");
+  static const thumbUp = IconData(0xe8dc, fontFamily: "MaterialSymbols");
+  static const timer = IconData(0xe425, fontFamily: "MaterialSymbols");
+  static const trendingDown = IconData(0xe8e3, fontFamily: "MaterialSymbols");
+  static const trendingFlat = IconData(0xe8e4, fontFamily: "MaterialSymbols");
+  static const trendingUp = IconData(0xe8e5, fontFamily: "MaterialSymbols");
+  static const tune = IconData(0xe429, fontFamily: "MaterialSymbols");
+  static const viewAgenda = IconData(0xe8e9, fontFamily: "MaterialSymbols");
+  static const widgets = IconData(0xe1bd, fontFamily: "MaterialSymbols");
+}
